0205ac47c023393e191df1d996b4efee5ae3ad0c
SoundarAathira <sounder.rla@gmail.com>
2022-11-07 09:20:36 +0530

    First Added

---
+++ src/test/java/com/git/TrialGit.java
@@ -0,0 +1,14 @@
+package com.git;
+
+public class TrialGit {
+	public void test1() {
+		System.out.println("1");
+
+	}
+	public void test2() {
+        System.out.println("2");
+	}
+    public void test3() {
+        System.out.println("3");
+	}
+}
